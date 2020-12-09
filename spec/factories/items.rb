123b47aab48e3ce1bc2682ebf77fff3ddae3122d
Saki9120 <0xv5pt193880r3x@gmail.com>
2020-12-09 20:59:06 +0900

    itemモデル単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image           { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.png')) }
+    name            {"aaaaaa"}
+    description     {"aaaaaa"}
+    price           {"2000"}
+    category_id     {"2"}
+    condition_id    {"2"}
+    postage_id      {"2"}
+    prefecture_id   {"2"}
+    days_to_ship_id {"2"}
+    association :user 
   end
 end
